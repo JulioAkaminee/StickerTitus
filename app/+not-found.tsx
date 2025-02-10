f069c4a084474217f31c5391409009d8826c4a7c
JulioAkaminee <juliooakamine@gmail.com>
2025-02-10 11:35:36 -0300

    feat: create navigation tabs

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -3,16 +3,15 @@ import React from "react";
 import { Link, Stack } from "expo-router";
 
 export default function NotFoundScreen() {
-  return (
-    <>
-    <Stack.Screen options={{title:"Oops! Not Found."}}/>
-    <View style={styles.container}>
-    <Text style={styles.title}>Error 404</Text>
-    <Link href="/" style={styles.button}>Go To Home</Link>
-    </View>
-    </>
-  );
-}
+    return (
+      <>
+        <View style={styles.container}>
+          <Text style={styles.title}>Error 404</Text>
+          <Link href="/" style={styles.button}>Go To Home</Link>
+        </View>
+      </>
+    );
+  }
 
 const styles = StyleSheet.create({
   container: {
